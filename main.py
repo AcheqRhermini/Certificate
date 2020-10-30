791c29862a879674610fdb74d184ea239c44a50f
Rhermini Acheq <arhermini@MacBook-Pro-de-Rhermini.local>
2020-10-30 16:51:45 +0100

    last commit

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-import json
-import os
-import csv
-import base64
 from fastapi import FastAPI,Form, Response
-from fastapi.encoders import jsonable_encoder
 import uvicorn
-import pymysql
+
 from reportlab.pdfgen.canvas import Canvas
+from reportlab.graphics import renderPDF
 from PyPDF2 import PdfFileReader
-#import date
+
+
+import qrcode
+import qrcode.image.svg
+from svglib.svglib import svg2rlg
+import tempfile
+
 import datetime
 import time
+
+def make_qr_code_drawing(data, size):
+    qr = qrcode.QRCode(
+        version=2,  # QR code version a.k.a size, None == automatic
+        error_correction=qrcode.constants.ERROR_CORRECT_H,  # lots of error correction
+        box_size=size,  # size of each 'pixel' of the QR code
+        border=4  # minimum size according to spec
+        )
+    qr.add_data(data)
+    qrcode_svg = qr.make_image(image_factory=qrcode.image.svg.SvgPathFillImage)
+    svg_file = tempfile.NamedTemporaryFile()
+    qrcode_svg.save(svg_file)  # store as an SVG file
+    svg_file.flush()
+    qrcode_rl = svg2rlg(svg_file.name)  # load SVG file as reportlab graphics
+    svg_file.close()
+    return qrcode_rl
+
 app = FastAPI()
 
 @app.route("/")
@@ -50,16 +69,21 @@ async def generate_pdf_certificate():
     canvas.drawString(70,230,"[  ]  Déplacements liés à des transits pour des déplacements de longues distances")
     canvas.drawString(70,190,"[ X ]  Déplacements brefs, dans un rayon maximal d'un kilomètre autour du domicile pour les")
     canvas.drawString(70,175,"       besoins des animaux de compagnie")
-    canvas.drawString(70,135,"Fait : Paris")
+    canvas.drawString(70,135,"Fait à : Paris")
+    #canvas.drawString(250,145,url)
     canvas.drawString(70,115,"Le : {} ".format(datetime.date.today()))
     time=str(datetime.datetime.now().time()).split(':')[0]+':'+str(datetime.datetime.now().time()).split(':')[1]
     canvas.drawString(300,115,"à : {} ".format(time))
     canvas.drawString(70,95,"signature:")
+    qrcode_rl = make_qr_code_drawing('www.google.com', 15)
+    renderPDF.draw(qrcode_rl,canvas, 400,30)
+
     canvas.save()
     with open("Attestation_deplacement.pdf", "rb") as f: 
         pdf= f.read() 
     reponse= Response(content=pdf, media_type="application/pdf") 
     return reponse
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=5000, log_level="info")
