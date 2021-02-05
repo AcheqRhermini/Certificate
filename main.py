64a1864f1000abbaed379ccd9e6128242528353d
AcheqRhermini <acheq.rhermini@gmail.com>
2021-02-05 08:57:35 +0000

    test jenkins pipe

--- main.py
+++ main.py
@@ -197,11 +197,11 @@ async def generate_pdf_certificate():
     canvas.drawString(70,325,"[  ]  Déplacements pour répondre à une convocation judiciaire ou administrative")
     canvas.drawString(70,285,"[  ]  Déplacements pour participer à des missions d'intérêt général sur demande de l'autorité")
     canvas.drawString(70,270,"       administrative")
-    #canvas.drawString(70,230,"[  ]  Déplacements liés à des transits pour des déplacements de longues distances")
+    canvas.drawString(70,230,"[  ]  Déplacements liés à des transits pour des déplacements de longues distances")
     canvas.drawString(70,190,"[ X ]  Déplacements brefs, dans un rayon maximal d'un kilomètre autour du domicile pour les")
     canvas.drawString(70,175,"       besoins des animaux de compagnie")
     canvas.drawString(70,135,"Fait à : Paris")
-    #canvas.drawString(250,145,url)
+    canvas.drawString(250,145,url)
     canvas.drawString(70,115,"Le : {} ".format(datetime.date.today()))
     time=str(datetime.datetime.now().time()).split(':')[0]+':'+str(datetime.datetime.now().time()).split(':')[1]
     canvas.drawString(300,115,"à : {} ".format(time))
